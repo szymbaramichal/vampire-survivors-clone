f6e29daaaf84b7a34cfb290c2f2e724af5d5f6c4
szymbaramichal <szymbaramichal@gmail.com>
2024-06-10 13:27:42 +0200

    Collisions basics

--- code/main.py
+++ code/main.py
@@ -1,6 +1,9 @@
 import pygame
 from settings import *
 from player import Player
+from sprites import *
+
+from random import randint
 
 class Game:
     def __init__(self):
@@ -13,8 +16,15 @@ class Game:
 
         # Groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
 
-        self.player = Player((400, 300), self.all_sprites)
+        #sprites
+        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
+
+        for i in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            CollisionSprite((x,y), (w,h), (self.all_sprites, self.collision_sprites))
 
     def run(self):
         while self.running:
--- code/player.py
+++ code/player.py
@@ -3,14 +3,16 @@ from settings import *
 from os.path import join
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_rect(center=pos)
-    
+        self.hitbox_rect = self.rect.inflate(-55, 0) #to remove blank space 
+
         # Movement
         self.direction = pygame.Vector2(0, 0)
         self.speed = 500
+        self.collision_sprites = collision_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -22,16 +24,26 @@ class Player(pygame.sprite.Sprite):
         if self.direction.length() > 0:
             self.direction = self.direction.normalize()
 
-        # Debugging output
-        print(f"Direction: {self.direction}")
-
     def move(self, dt):
         # Update position based on direction and speed
         if self.direction.length() > 0:
-            self.rect.center += self.direction * self.speed * dt
+            self.hitbox_rect.x += self.direction.x * self.speed * dt
+            self.collision('horizontal')
+            self.hitbox_rect.y += self.direction.y * self.speed * dt
+            self.collision('vertical')
+            self.rect.center = self.hitbox_rect.center
 
-        # Debugging output
-        print(f"Position: {self.rect.center}")
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+                    
 
     def update(self, dt):
         self.input()
