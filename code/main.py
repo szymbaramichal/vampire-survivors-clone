71ac5b34da390c3646bb850fb3e367ea1c7ecb78
szymbaramichal <szymbaramichal@gmail.com>
2024-06-11 12:39:11 +0200

    Shooting

--- code/main.py
+++ code/main.py
@@ -15,11 +15,34 @@ class Game:
         pygame.display.set_caption('Vampire Survivor clone')
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
 
         # Groups
         self.all_sprites = AllSprites()
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
         self.setup()
+
+        #gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 500
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 30
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('images', 'gun', 'bullet.png')).convert_alpha()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
 
     def run(self):
         while self.running:
@@ -32,6 +55,8 @@ class Game:
                     self.running = False
 
             # Update
+            self.gun_timer()
+            self.input()
             self.all_sprites.update(dt)
 
             # Draw
@@ -56,6 +81,7 @@ class Game:
         for obj in map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
 if __name__ == '__main__':
     game = Game()
--- code/sprites.py
+++ code/sprites.py
@@ -1,4 +1,5 @@
 from settings import *
+from math import atan2, degrees
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -12,3 +13,49 @@ class CollisionSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft=pos)
+
+class Gun(pygame.sprite.Sprite):
+    def __init__(self, player, groups):
+        self.player = player
+        self.distance = 140
+        self.player_direction = pygame.Vector2(1,0)
+
+        super().__init__(groups)
+        self.gun_surf = pygame.image.load(join('images', 'gun', 'gun.png')).convert_alpha()
+        self.image = self.gun_surf
+        self.rect = self.image.get_rect(center = self.player.rect.center + self.player_direction * self.distance)
+
+    def get_direction(self):
+        mouse_pos = pygame.Vector2(pygame.mouse.get_pos())
+        player_pos = pygame.Vector2(WINDOW_WIDTH / 2, WINDOW_HEIGHT /2)
+        self.player_direction = (mouse_pos - player_pos).normalize()
+
+    def rotate_gun(self):
+        angle = degrees(atan2(self.player_direction.x, self.player_direction.y)) - 90
+        if self.player_direction.x > 0:
+            self.image = pygame.transform.rotozoom(self.gun_surf, angle, 1)
+        else:
+            self.image = pygame.transform.rotozoom(self.gun_surf, abs(angle), 1)
+            self.image = pygame.transform.flip(self.image, False, True)
+
+    def update(self, _):
+        self.get_direction()
+        self.rotate_gun()
+        self.rect.center = self.player.rect.center + self.player_direction * self.distance
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, direction, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(center=pos)
+        self.spawn_time = pygame.time.get_ticks()
+        self.lifetime = 1500
+
+        self.direction = direction
+        self.speed = 1000
+
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+
+        if pygame.time.get_ticks() - self.spawn_time >= self.lifetime:
+            self.kill()
